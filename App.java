2ebd54a9c2a61b6a69b749061b0cee5b791a17ea
Edward <leonardo071123@gmail.com>
2024-10-22 11:49:48 +0800

    Update App.java
    增加說明註解

--- App.java
+++ App.java
@@ -13,34 +13,40 @@ public class App {
          System.out.println(testString);
     }
 
-
+    // 有傳入參數
     public static String dev(int num1, int num2) {
         String rTest = "test";
         return rTest;
     }
 
+    // 無傳入參數
     public static String test1() {
         String rTest = "test";
         return rTest;
     }
 
+    // 不公開
     private static String test12() {
         String rTest = "test";
         return rTest;
     }
 
+    // 無前贅字 
     static String test13() {
         return "test13";
     }
 
+    // 無前贅字 無靜態關鍵字
     String test14() {
         return "test14";
     }
 
+    // 保護且無靜態關鍵字
     protected String test15() {
         return "test15";
     }
 
+    // 無須回傳值
     public void test2() {
         String rTest = "test";
     }
